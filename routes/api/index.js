8c1e1c16721365f35d2567889618398039dc024d
Devin <dzimmerman902@gmail.com>
2018-08-15 09:19:23 -0600

    Basic template for Album routes/controllers, found a small bug when updating which I can get to later

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const albumRoutes = require('./album');
 const userRoutes = require('./user');
 
+router.use('/album', albumRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;

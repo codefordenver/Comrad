76f119144c917b18bc36788f78a1df68830bbdae
Devin Zimmerman <dzimmerman902@gmail.com>
2018-08-20 09:32:06 -0600

    Finished User Routes

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -24,7 +24,7 @@ module.exports = {
 
   update: (req, res) => {
     db.Track
-      .findOneAndUpdate({ _id: req.params.id }, req.body, { rawResult: true })
+      .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
       .then(dbTrack => res.json(dbTrack))
       .catch(err => res.status(422).json(err));
   },

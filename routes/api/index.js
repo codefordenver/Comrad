5df74c7731f961bbfe544ffe6ec71b41548db1ae
Devin Zimmerman <dzimmerman902@gmail.com>
2018-08-20 18:03:24 -0600

    Created Routes/Controller/Model for Venue

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const albumRoutes = require('./album');
 const trackRoutes = require('./track');
 const userRoutes = require('./user');
+const venueRoutes = require('./venue');
 
 router.use('/album', albumRoutes);
 router.use('/track', trackRoutes);
 router.use('/user', userRoutes);
+router.use('/venue', venueRoutes);
 
 module.exports = router;

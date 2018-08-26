0a4c1f014490336f2096972a9d12435119f776d6
Devin Zimmerman <dzimmerman902@gmail.com>
2018-08-26 11:31:48 -0600

    Created seed file for starter album/song data

--- scripts/seedDB/album.js
+++ scripts/seedDB/album.js
@@ -2,43 +2,205 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const db = require('../../models');
 const keys = require('../../config/keys');
-const tracks = require('./track');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
-const albumData = {
-  itunes_id: '1',
-  title: 'Thriller',
-  artist: 'Michael Jackson',
-  label: 'Epic',
-  genre_id: '1',
-  local: false,
-  compilation: '',
-  location: '',
-  album_art: '',
-  tracks: []
-}
+const seeds = [
+  {
+    album: {
+      title: 'Escape',
+      artist: 'Journey',
+      label: 'Columbia',
+      local: false,
+      compilation: '',
+      location: '',
+      album_art: ''
+    },
+    tracks: [
+      {
+        title: 'Don\'t Stop Believin',
+        track_number: '1',
+        disk_number: '1',
+        duration: '4:11',
+        artist: 'Journey'
+      },
+      {
+        title: 'Stone in Love',
+        track_number: '2',
+        disk_number: '1',
+        duration: '4:26',
+        artist: 'Journey'
+      },
+      {
+        title: 'Who\'s Crying Now',
+        track_number: '3',
+        disk_number: '1',
+        duration: '5:01',
+        artist: 'Journey'
+      },
+      {
+        title: 'Keep On Runnin',
+        track_number: '4',
+        disk_number: '1',
+        duration: '3:40',
+        artist: 'Journey'
+      },
+      {
+        title: 'Still They Ride',
+        track_number: '5',
+        disk_number: '1',
+        duration: '3:50',
+        artist: 'Journey'
+      },
+      {
+        title: 'Escape',
+        track_number: '6',
+        disk_number: '2',
+        duration: '5:17',
+        artist: 'Journey'
+      },
+      {
+        title: 'Lay It Down',
+        track_number: '7',
+        disk_number: '2',
+        duration: '4:13',
+        artist: 'Journey'
+      },
+      {
+        title: 'Dead or Alive',
+        track_number: '8',
+        disk_number: '2',
+        duration: '3:21',
+        artist: 'Journey'
+      },
+      {
+        title: 'Mother, Father',
+        track_number: '9',
+        disk_number: '2',
+        duration: '5:29',
+        artist: 'Journey'
+      },
+      {
+        title: 'Open Arms',
+        track_number: '10',
+        disk_number: '2',
+        duration: '3:23',
+        artist: 'Journey'
+      }
+    ]
+  },
+  {
+    album: {
+      title: 'Thriller',
+      artist: 'Michael Jackson',
+      label: 'Epic',
+      local: false,
+      compilation: '',
+      location: '',
+      album_art: ''
+    },
+    tracks: [
+      {
+        title: 'Wanna Be Startin\' Something',
+        track_number: '1',
+        disk_number: '1',
+        duration: '6:02',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'Baby Be Mine',
+        track_number: '2',
+        disk_number: '1',
+        duration: '4:20',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'The Girl Is Mine',
+        track_number: '3',
+        disk_number: '1',
+        duration: '3:41',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'Thriller',
+        track_number: '4',
+        disk_number: '1',
+        duration: '5:57',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'Beat It',
+        track_number: '5',
+        disk_number: '1',
+        duration: '4:18',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'Billie Jean',
+        track_number: '6',
+        disk_number: '2',
+        duration: '4:54',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'Human Nature',
+        track_number: '7',
+        disk_number: '2',
+        duration: '4:07',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'P.Y.T. (Pretty Young Thing)',
+        track_number: '8',
+        disk_number: '2',
+        duration: '3:58',
+        artist: 'Michael Jackson'
+      },
+      {
+        title: 'The Lady In My Life',
+        track_number: '9',
+        disk_number: '2',
+        duration: '4:59',
+        artist: 'Michael Jackson'
+      }
+    ]
+  }
+]
 
 db.Album
   .remove({})
-  .then(()=> db.Album.create(albumData))
-  .then(dbAlbum => {
-    console.log(dbAlbum);
+  .catch(err => res.json(err));
 
-    tracks.forEach(track => {
-      return track.album_id = dbAlbum._id;
-    });
+db.Track
+  .remove({})
+  .catch(err => res.json(err));
 
-    console.log(tracks);
+seeds.map(seed => {
+  const { album, tracks } = seed;
 
-    db.Track
-      .insertMany(tracks, (err, dbTrack) => {
-        console.log(dbTrack);
+  db.Album
+    .create(album)
+    .then(dbAlbum => {
+
+      const trackPromise = tracks.map(track => {
+
+        track['album_id'] = dbAlbum._id;
+
+        return db.Track
+          .create(track)
+          .then(dbTrack => {
+            dbAlbum.tracks.push(dbTrack._id)
+          })
+          .catch(err => res.status(422).json(err));
       });
 
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1)
-  });
+      Promise.all(trackPromise).then(() => {
+        dbAlbum
+          .save()
+          .then(dbAlbum => {
+            console.log(dbAlbum);
+          })
+          .catch(err => res.status(422).json(err));
+      });
+    });
+});

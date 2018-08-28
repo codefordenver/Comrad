d3b1c4851632937329373fad18d8a55d504e014a
dzimmerman902 <dzimmerman902@gmail.com>
2018-08-28 11:04:51 -0600

    Created starter routes for pages when server is running

---
+++ client/src/components/Form/Input.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Input = props => (
+  <input className="form__input" />
+);
---
+++ client/src/pages/User/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        User Component
+      </div>
+    )
+  }
+}
+
+export default User

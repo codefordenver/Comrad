716f03805204c409550eadf2da25fd7a43c8b667
dzimmerman902 <dzimmerman902@gmail.com>
2018-08-29 11:14:57 -0600

    Started work on the Home page. This will be considered the landing page once someone goes to the site. Input and buttons were added as well as some styling

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+import logo from './kgnu_logo.png';
+
+import { Link, SubmitBtn } from '../../components/Button';
+import { Card, CardBody, CardImg, CardTitle } from '../../components/Card';
+import { Form, FormGroup, Input, Label } from '../../components/Form';
 
 class Home extends Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
-      <div>
-        Home Component        
+      <div id="home">
+        <section className="home__login">
+        <Card>
+            <img className="img img--home" src={logo} /> 
+            <CardBody helpers='text-center'>
+
+              <CardTitle>
+                COMRAD - KGNU PLAYLIST LOGIN:
+              </CardTitle>
+              
+              <Form>
+
+                <FormGroup>
+                  <Input
+                    name="username" 
+                    onChange={this.handleInputChange} 
+                    placeholder="Username"
+                    type="text"
+                    value={this.state.username}
+                  />
+                  <Label>Username</Label>
+                </FormGroup>
+
+                <FormGroup helpers="mb-3">
+                  <Input
+                    name="password"
+                    onChange={this.handleInputChange} 
+                    placeholder="Password"
+                    type="password"
+                    value={this.state.password}
+                  />
+                  <Label>Password</Label>
+                </FormGroup>
+
+                <SubmitBtn>Sign In</SubmitBtn>
+                <Link link="#">
+                  Reset Password
+                </Link>
+              </Form>
+            </CardBody>
+          </Card>
+        </section>
       </div>
     )
   }
 }
 
-export default Home
+export default connect(null, actions)(Home);

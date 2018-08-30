106f5a5b69a29849566dd58b748c9b8e75b7f00b
dzimmerman902 <dzimmerman902@gmail.com>
2018-08-30 14:52:53 -0600

    Updated root by adding initial state to help with testing

--- client/src/Root.js
+++ client/src/Root.js
@@ -3,10 +3,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-export default (props) => {
+export default ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>
-      {props.children}
+    <Provider store={createStore(reducers, initialState)}>
+      {children}
     </Provider>
   )
 }

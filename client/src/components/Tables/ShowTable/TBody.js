e7bcb1a97c38b2f7585a87a775d195fb450a4b45
Tyler <tyler@ogdenstudios.xyz>
2018-10-01 21:52:17 -0600

    add empty classes to all table elements

--- client/src/components/Tables/ShowTable/TBody.js
+++ client/src/components/Tables/ShowTable/TBody.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const TBody = props => {
   return (
-    <tbody>
+    <tbody className="table__body">
       {props.rows.map(row => (
-        <tr key={row.id}>
-          <td>{row.name}</td>
-          <td>{row.time}</td>
-          <td>Show Builder</td>
-          <td>Edit Show Details</td>
+        <tr className="table__row" key={row.id}>
+          <td className="table__cell">{row.name}</td>
+          <td className="table__cell">{row.time}</td>
+          <td className="table__cell">Show Builder</td>
+          <td className="table__cell">Edit Show Details</td>
         </tr>
       ))}
     </tbody>
--- client/src/components/Tables/ShowTable/THead.js
+++ client/src/components/Tables/ShowTable/THead.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const THead = props => {
   return (
     <thead className="table__header">
-      <tr>
+      <tr className="table__row">
         {props.columns.map(column => (
           <th className="table__header__cell" key={column.key}>{column.name}</th>
         ))}

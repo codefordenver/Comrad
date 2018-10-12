8e6ae77ad1058de9c10de68e5f4090a43a9ecc16
dzimmerman902 <dzimmerman902@gmail.com>
2018-10-12 13:30:00 -0600

    Started working on Search Bar

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,5 @@
 export const AUTH_SIGNIN = 'auth_signin';
 export const AUTH_SIGNOUT = 'auth_signout';
 export const AUTH_ERROR = 'auth_error';
+
+export const SEARCH_ALL = 'search_all';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+
 import auth from './auth';
+import search from './search';
 
 export default combineReducers({
   auth,
+  search
 })

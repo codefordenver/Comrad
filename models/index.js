ce91630c4f7e6c8fad40af320a501a9400ffa2f2
Josh Bernstein <joshua.bernstein@outlook.com>
2018-10-15 21:51:17 -0600

    Up to date with origin/development

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   Album: require('./album'),
   Announcement: require('./announcement'),
+  Artist: require('./artist'),
   Event: require('./event'),
   Feature: require('./feature'),
   Giveaway: require('./giveaway'),

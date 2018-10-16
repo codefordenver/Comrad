ad3f4b2e9029bdb48774514517d7279221a02742
dzimmerman902 <dzimmerman902@gmail.com>
2018-10-15 19:57:29 -0600

    Added Albums and Tracks components to development

---
+++ client/src/components/Albums/AddAlbum.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddAlbum extends Component {
+  render() {
+    return (
+      <div>
+        Add Album        
+      </div>
+    )
+  }
+}
+
+export default AddAlbum;

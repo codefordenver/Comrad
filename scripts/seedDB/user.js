7fe6b11cd4c68fad793c036211628f1befc0128e
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-22 19:30:13 -0600

    switch to create method for mongodb on the user seed file

--- scripts/seedDB/user.js
+++ scripts/seedDB/user.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const db = require('../../models');
 const keys = require('../../config/keys');
+const userSeeds = require('./lib/users.json');
+
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
@@ -12,23 +14,7 @@ db.User.deleteMany({"email": {"$ne": "comrad.development@gmail.com"}}, function 
   if (err) return handleError(err);
 });
 
-var jsonArr = [];
 
-
-for (i = 0; i < 1000; i++) {
-  jsonArr.push({
-    "email": "example" + i + "@example.com",
-    "password": "password"
-  });
-}
-
-
-// insertMany is fast enough to insert 1000 entries, but it doesn't "create" them
-// so it bypasses encrypting the passwords
-// This is fine for populating the db and testing out the API, but if we want to
-// populate with working users and passwords, we need to use the "create" method
-// on db.User - however, that's so slow the connection times out.
-// Might want to reset timeout value for mongoose 
-db.User.insertMany(jsonArr, function(err) {
-    if (err) return handleError(err);
+userSeeds.forEach(seed => {
+  db.User.create(seed).catch(err => console.log(err));
 });

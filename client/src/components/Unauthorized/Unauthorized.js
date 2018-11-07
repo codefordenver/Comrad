3db148de0995addbbfd124c8211decfc0ce9c3d8
Tyler Williams <tyler@ogdenstudios.xyz>
2018-11-06 19:53:08 -0700

    add placeholder unauthorized component

---
+++ client/src/components/Unauthorized/Unauthorized.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Unauthorized extends Component {
+  render() {
+    return (
+      <div>
+        Unauthorized
+      </div>
+    )
+  }
+}
+
+export default Unauthorized

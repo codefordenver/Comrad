141241a0af09dd073406778594cf52cd71b822e0
Tyler Williams <tyler@ogdenstudios.xyz>
2018-11-06 20:18:18 -0700

    center unauthorized header and placeholder div using flexbox - still need to replace unauthorized alert with alerts from #218

--- client/src/components/Unauthorized/Unauthorized.js
+++ client/src/components/Unauthorized/Unauthorized.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react'
 class Unauthorized extends Component {
   render() {
     return (
-      <div>
+      <div className="unauthorized">
+      <div className="unauthorized__wrapper">
         <h1 className="unauthorized__h1">401: Unauthorized Access</h1>
-        <div>
+        // The unauthorized__placeholder should be replaced by the alert
+        // component from Issue #218 when ready 
+        <div className="unauthorized__placeholder">
         Alert button with unauthorized text goes here
+        </div>
         </div>
       </div>
     )

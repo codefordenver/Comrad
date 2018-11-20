a5968fad355a448120f731763e6f25e42b61e8b6
crodier1 <crodier@protonmail.com>
2018-11-19 20:39:27 -0700

    refactor alert component

--- client/src/pages/Admin/AdminPage.js
+++ client/src/pages/Admin/AdminPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Alert from '../../components/Alerts/Alert';
 
 class AdminPage extends Component {
   state = {};
@@ -7,7 +8,18 @@ class AdminPage extends Component {
     return (
       <div className="admin__page">
         Admin Page
+
+        <Alert alertType="warning" title="Warning">Warning Will Robinson, Warning!</Alert>
+
+        <Alert alertType="error" title="Error">Satan will eat your brain!!</Alert>
+
+        <Alert alertType="success" title="Success">You've learn now to spell "error", congrats!</Alert>
+
+        <Alert alertType="info" title="Information">Ipsom lerom blah blah blah</Alert>
+
+
       </div>
+
     )
   }
 }

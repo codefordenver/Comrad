50427e0dbdbd7fa7ff01d0f5d5d60a0f39fe28a3
Josh Bernstein <joshua.bernstein@outlook.com>
2018-11-29 22:06:16 -0700

    Added basic event routes and event reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import search from './search';
 import user from './user';
+import events from './event';
 
 export default combineReducers({
   auth,
+  events,
   search,
   user
 });

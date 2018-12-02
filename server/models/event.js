b04df61d3650e8e066dbc44b106f47903ac48d70
Josh Bernstein <joshua.bernstein@outlook.com>
2018-12-02 10:11:33 -0700

    Updated model event.js

--- server/models/event.js
+++ server/models/event.js
@@ -2,42 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  master_event_uid:   Number,
-  status:             String,
-  start_date_utc:     Date,
-  end_date_utc:       Date,
-  duration:           Number,
-  is_recurring:       Boolean,
-  repeat_rule:{
-    frequency:        String,
-    dtstart:          Date,
-    until:            Date,
-    count:            Number,
-    byweekly:         String,
-    bymonth:          String
-  },
-  
-  repeat_date:        Date,
-  exclude_rule:       String,
-  exclude_date:       Date,
+  status:               String,
   event_details: {
-    title:            String,
-    summary:          String,
-    description:      String,
-    producer:         String,
-    host:             String,
-    guest:            [String],
-    custom:           String
+    title:              String,
+    summary:            String,
+    description:        String,
+    producer:           String,
+    host:               String,
+    guests:             [String],
+    custom:             String
   },
 
+  event_start_time_utc: Date,
+  event_end_time_utc:   Date,
+  duration_in_minutes:  Number,
+
+  master_event_uid:     Number,
+  replace_event_date:   Date,
+
+  is_recurring:         Boolean,
+  repeat_rule:{
+    frequency:          String,
+    repeat_start_date:  Date,
+    repeat_end_date:    Date,
+    count:              Number,
+    byweekly:           String,
+    bymonth:            String
+  },
+  
+  exclude_rule:         String,
+  exclude_dates:        [Date],
+  
   created_at: {
-    type: Date,
-    default: Date.now
+    type:               Date,
+    default:            Date.now
   },
 
   updated_at: {
-    type: Date,
-    default: Date.now
+    type:               Date,
+    default:            Date.now
   }
 });
 

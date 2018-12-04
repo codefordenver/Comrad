a74473345f32da4e3c8c38b153aef0d238f5575c
Devin Zimmerman <dzimmerman902@gmail.com>
2018-12-04 10:41:04 -0700

    Incorporated Lauren's footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer__text">
+      <img
+        className="footer__logo"
+        src="https://i.postimg.cc/dVFWp5Fq/Comrad-Logo-13.png"
+      />
+    </p>
+  </footer>
+)
+
+export default Footer

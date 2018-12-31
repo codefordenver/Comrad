359acf1194684f7687f03c6a7b3a657c0aaead71
Devin Zimmerman <dzimmerman902@gmail.com>
2018-12-30 18:26:17 -0700

    Removed Card components, will create some more efficient later

--- client/src/components/Card/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = props => {
-  const { children, styleName } = props;
-
-  return <div className={`card ${styleName || ''}`}>{children}</div>;
-};
-
-export default Card;
--- client/src/components/CardBody/CardBody.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CardBody = props => {
-  const { children, className } = props;
-
-  return <div className={`card__body ${className || ''}`}>{children}</div>;
-};
-
-export default CardBody;

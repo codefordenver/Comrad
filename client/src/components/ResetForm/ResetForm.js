e824e9aeee909fd22ac4873fb256f97168f38350
Devin Zimmerman <dzimmerman902@gmail.com>
2019-01-03 13:56:45 -0700

    Updated ResetForm component

--- client/src/components/ResetForm/ResetForm.js
+++ client/src/components/ResetForm/ResetForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { resetUser } from '../../actions';
+import PropTypes from 'prop-types';
 import { REGEX_EMAIL } from '../../utils/validation';
+import { requestReset } from '../../actions';
 
 import Alert from '../../components/Alert';
 import Button from '../../components/Button';
@@ -11,11 +12,12 @@ import Input from '../../components/Input';
 
 class ResetForm extends Component {
   render() {
-    const { message, resetUser } = this.props;
+    const { requestReset, user } = this.props;
 
     return (
-      <Form action={resetUser}>
+      <Form action={requestReset}>
         <Alert />
+        <p>Enter Email Address</p>
         <FormGroup>
           <Input
             name="email"
@@ -23,24 +25,30 @@ class ResetForm extends Component {
             feedback="Please Enter Email Address"
             label="Email"
             validate={REGEX_EMAIL}
+            disabled={user.loading}
           />
         </FormGroup>
 
         <Button color="primary" type="submit">
-          Submit
+          {user.loading ? 'Loading' : 'Submit'}
         </Button>
       </Form>
     );
   }
 }
 
+ResetForm.propTypes = {
+  resetForm: PropTypes.func,
+};
+
 function mapStateToProps(state) {
   return {
     message: state.message,
+    user: state.user,
   };
 }
 
 export default connect(
   mapStateToProps,
-  { resetUser },
+  { requestReset },
 )(ResetForm);

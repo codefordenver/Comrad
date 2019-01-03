0b236ec2f007a5020e95c8639ac925c3653920ed
Devin Zimmerman <dzimmerman902@gmail.com>
2019-01-03 13:55:28 -0700

    Added propTypes and logic to link buttons

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Button extends Component {
   render() {
-    const { children, color, onClick, type } = this.props;
+    const {
+      children,
+      color,
+      disabled,
+      onClick,
+      styleName,
+      to,
+      type,
+    } = this.props;
+    if (to) {
+      return (
+        <Link
+          disabled={disabled}
+          to={to}
+          className={`button button--link ${styleName}`}
+          onClick={onClick}
+          type={type}
+        >
+          {children}
+        </Link>
+      );
+    }
 
     return (
       <button
-        className={`button button--${color}`}
+        className={`button button--${color} ${styleName}`}
+        disabled={disabled}
         onClick={onClick}
         type={type}
       >
@@ -26,6 +49,7 @@ Button.propTypes = {
    * Change the color
    */
   color: PropTypes.string,
+  disabled: PropTypes.bool,
   /**
    * Add an on click function to the component
    */
@@ -33,13 +57,18 @@ Button.propTypes = {
   /**
    * Set the type for the button
    */
+  styleName: PropTypes.string,
+  to: PropTypes.string,
   type: PropTypes.string,
 };
 
 Button.defaultProps = {
   children: null,
   color: 'primary',
+  disabled: false,
   onClick: null,
+  styleName: '',
+  to: '',
   type: 'button',
 };
 

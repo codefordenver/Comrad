9b91c80775949b8054962d4a2f159f0ce767a3eb
Devin Zimmerman <dzimmerman902@gmail.com>
2019-01-03 14:01:04 -0700

    Created individual pages for all the Password form components. Did some more clean up of need home page.

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -3,117 +3,29 @@ import logo from '../../images/kgnu_logo.png';
 
 import Button from '../../components/Button';
 import LoginForm from '../../components/LoginForm';
-import ResetForm from '../../components/ResetForm';
-import SignupForm from '../../components/SignupForm';
 
 class LoginPage extends Component {
-  state = {
-    display: 'login',
-  };
-
-  handleDisplayAction = display => {
-    console.log(display);
-    this.setState({
-      display,
-    });
-  };
-
-  renderLoginForm() {
+  render() {
     return (
-      <Fragment>
-        <p>Login to your account</p>
-        <div className="login__form">
+      <div className="login-page">
+        <div className="login-page__form">
           <LoginForm {...this.props} />
         </div>
-        <div className="login__reset">
-          <Button
-            onClick={() => this.handleDisplayAction('reset')}
-            color="link"
-          >
-            Reset Password
-          </Button>
+        <div className="login-page__reset-button">
+          <Button to="/reset">Reset Password</Button>
         </div>
-        <div className="login__new-user">
+        <div className="login-page__new-user">
           <p>
             <b>New to Comrad?</b>
           </p>
+
           <p>Sign Up for your free account!</p>
 
-          <Button
-            onClick={() => this.handleDisplayAction('signup')}
-            color="primary"
-          >
+          <Button to="/signup" color="primary">
             Sign Up
           </Button>
         </div>
-      </Fragment>
-    );
-  }
-
-  renderResetForm() {
-    return (
-      <Fragment>
-        <p>Enter Your Email Address</p>
-        <div className="login__form">
-          <ResetForm />
-        </div>
-        <div className="login__go-back">
-          <Button
-            onClick={() => this.handleDisplayAction('login')}
-            color="link"
-          >
-            Go Back
-          </Button>
-        </div>
-      </Fragment>
-    );
-  }
-
-  renderSignupForm() {
-    return (
-      <Fragment>
-        <p>Enter Information</p>
-        <div className="login__form">
-          <SignupForm />
-        </div>
-        <div className="login__go-back">
-          <Button
-            onClick={() => this.handleDisplayAction('login')}
-            color="link"
-          >
-            Go Back
-          </Button>
-        </div>
-      </Fragment>
-    );
-  }
-
-  renderDisplay() {
-    const { display } = this.state;
-
-    switch (display) {
-      case 'login':
-        return this.renderLoginForm();
-      case 'reset':
-        return this.renderResetForm();
-      case 'signup':
-        return this.renderSignupForm();
-      default:
-        break;
-    }
-  }
-
-  render() {
-    return (
-      <main className="login">
-        <section className="login__left" />
-        <section className="login__right">
-          <div className="login__logos">
-            <img className="login__kgnu" src={logo} alt="KGNU" />
-          </div>
-          {this.renderDisplay()}
-        </section>
-      </main>
+      </div>
     );
   }
 }

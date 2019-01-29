adbb0a575b7f05068c90a739b5ac720adb80af43
Devin Zimmerman <dzimmerman902@gmail.com>
2019-01-28 21:44:34 -0700

    Added font awesome back into project

--- client/src/components/DropdownUser/DropdownUser.js
+++ client/src/components/DropdownUser/DropdownUser.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import { Link } from 'react-router-dom';
+
+export const DropdownUserItem = props => {
+  const { text, to } = props;
+
+  return (
+    <li className="dropdown-user__item">
+      <Link to={to} className="dropdown-user__link">
+        {text}
+      </Link>
+    </li>
+  );
+};
 
 class DropdownUser extends Component {
   state = {
@@ -13,15 +26,17 @@ class DropdownUser extends Component {
   };
 
   render() {
+    const { children, className, ...rest } = this.props;
+
     return (
-      <div className="dropdown-user">
+      <div className={classnames('dropdown-user', className)} {...rest}>
         <div className="dropdown-user__icon">
-          <
+          <i className="fas fa-user" />
         </div>
-        <ul className="dropdown-user__list">
-          {children}
-        </ul>
+        <ul className="dropdown-user__list">{children}</ul>
       </div>
-    )
+    );
   }
 }
+
+export default DropdownUser;

3d77fba7af1b9d333b576612f5f946c73d32bae7
Devin Zimmerman <dzimmerman902@gmail.com>
2019-02-27 13:11:09 -0700

    Removed Actions and Reducers folder

--- client/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export * from './artist';
-export * from './auth';
-export * from './library';
-export * from './modals';
-export * from './shows';
-export * from './traffic';
-export * from './user';

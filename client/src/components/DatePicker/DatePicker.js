0de8c5efe81878cf9622e8ca9e3d593e507e0667
dzimmerman902 <dzimmerman902@gmail.com>
2019-04-01 06:59:44 -0600

    update(DatePicker): Syntax updates

--- client/src/components/DatePicker/DatePicker.js
+++ client/src/components/DatePicker/DatePicker.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { InputLabel } from '../Input/Input';
-import { SingleDatePicker } from 'react-dates';
 import moment from 'moment';
+
+import { SingleDatePicker } from 'react-dates';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
+import { InputLabel } from '../Input';
+
 class DatePicker extends Component {
-  state = { focused: false };
+  state = {
+    date: '',
+    focused: false,
+  };
 
   componentDidMount() {
     const { value } = this.props.input;
@@ -17,30 +22,39 @@ class DatePicker extends Component {
     }
   }
 
+  handleDateChange = date => {
+    const { allowNullDate = false, input } = this.props;
+    const { onChange } = input;
+
+    if (moment(date).isValid() || allowNullDate) {
+      onChange(date);
+      this.setState({ date });
+    }
+  };
+
+  handleFocusChange = ({ focused }) => {
+    this.setState({ focused });
+  };
+
   render() {
+    const { handleDateChange, handleFocusChange, props, state } = this;
     const {
-      input: { name, onChange, value },
-      meta,
-      label,
       className,
-      allowNullDate = false,
+      input: { name, value },
+      label,
+      meta,
       ...rest
-    } = this.props;
-    const { focused } = this.state;
+    } = props;
+    const { focused } = state;
 
     return (
       <div className={classnames('form-group', className)}>
         <SingleDatePicker
           id={`${name}_picker`} // PropTypes.string.isRequired,
           date={value}
-          onDateChange={date => {
-            if (moment(date).isValid() || allowNullDate) {
-              onChange(date);
-              this.setState({ date });
-            }
-          }} // PropTypes.func.isRequired
+          onDateChange={handleDateChange} // PropTypes.func.isRequired
           focused={focused} // PropTypes.bool
-          onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+          onFocusChange={handleFocusChange} // PropTypes.func.isRequired
           isOutsideRange={() => false}
           numberOfMonths={1}
           small

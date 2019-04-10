ae2b64cbe8fe81f8b478f57bb2fd40f1792ce1fb
Kyle Middleton <kylemid321@gmail.com>
2019-04-09 18:44:08 -0600

    defined checkbox label as props.label

--- client/src/components/Checkbox/Checkbox.js
+++ client/src/components/Checkbox/Checkbox.js
@@ -3,17 +3,16 @@ import './_Checkbox.scss';
 
 const Checkbox = props => {
   const { className, ...rest } = props;
-  const label = 'test box'; //this will be passed in from parent component as props
   return (
     <div className="checkbox-container">
       <input
         type="checkbox"
-        name={label}
+        name={props.label}
         className={`check-box ${className || ''}`}
         {...rest}
       />
       <span className="checkmark" />
-      <div className="checkbox-label">{label}</div>
+      <div className="checkbox-label">{props.label}</div>
     </div>
   );
 };

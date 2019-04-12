606c4813cc97e0d748c5f667c4a708354bc781f2
Kyle Middleton <kylemid321@gmail.com>
2019-04-12 12:18:06 -0600

    changed component from stateless to stateful

--- client/src/components/Checkbox/Checkbox.js
+++ client/src/components/Checkbox/Checkbox.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './_Checkbox.scss';
 
-const Checkbox = props => {
-  const { className, ...rest } = props;
-  return (
-    <div className="checkbox-container">
-      <input
-        type="checkbox"
-        name={props.label}
-        className={`check-box ${className || ''}`}
-        {...rest}
-      />
-      <span className="checkmark" />
-      <div className="checkbox-label">{props.label}</div>
-    </div>
-  );
-};
+class Checkbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { className, ...rest } = this.props;
+    return (
+      <div className="checkbox-container">
+        <input
+          type="checkbox"
+          name={this.props.label}
+          className={`check-box ${className || ''}`}
+          {...rest}
+        />
+        <span className="checkmark" />
+        <div className="checkbox-label">{this.props.label}</div>
+      </div>
+    );
+  }
+}
 
 export default Checkbox;

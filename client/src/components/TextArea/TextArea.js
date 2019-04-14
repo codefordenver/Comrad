a7b80985ce38912ab9372953e3797ce76d960a8f
Kyle Middleton <kylemid321@gmail.com>
2019-04-14 14:37:28 -0600

    initial commit

---
+++ client/src/components/TextArea/TextArea.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+export class TextArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default TextArea;

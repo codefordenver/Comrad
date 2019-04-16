f0265d0f19578f00e4207f4d7030cd5d3e0e74be
Kyle Middleton <kylemid321@gmail.com>
2019-04-15 19:30:45 -0600

    initial commit

--- client/src/components/TextArea/TextArea.js
+++ client/src/components/TextArea/TextArea.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
+import moment from 'moment';
+import './_TextArea.scss';
 
 export class TextArea extends React.Component {
   constructor(props) {
@@ -8,7 +12,19 @@ export class TextArea extends React.Component {
   }
 
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <form>
+          <textarea
+            className="large-input"
+            type="text"
+            placeholder="test input"
+            rows="5"
+            cols="80"
+          />
+        </form>
+      </div>
+    );
   }
 }
 

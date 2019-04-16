c0d994be28b8e4091e47186227dd7561db21bb08
Kyle Middleton <kylemid321@gmail.com>
2019-04-16 15:27:04 -0600

    copied basic layout from input component

--- client/src/components/TextArea/TextArea.js
+++ client/src/components/TextArea/TextArea.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { connect } from 'react-redux';
-import { Field, reduxForm, formValueSelector } from 'redux-form';
-import moment from 'moment';
-import './_TextArea.scss';
 
-export class TextArea extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export const ICON_SET = {
+  search: <i className="icon fas fa-search" />,
+  user: <i className="icon fas fa-user" />,
+};
 
+export const TextAreaError = props => {
+  const { children } = props;
+  return <div className="input__error">{children}</div>;
+};
+
+export const TextAreaLabel = props => {
+  const {
+    active,
+    children,
+    dirty,
+    error,
+    initial,
+    touched,
+    dirtyOverride,
+  } = props;
+
+  return (
+    <div
+      className={classnames(
+        'textarea__label',
+        (active || initial) && 'active',
+        (dirty || dirtyOverride) && 'dirty',
+        touched && error && 'error',
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+class TextArea extends Component {
   render() {
+    const { props } = this;
+
+    const {
+      autoFocus,
+      className,
+      icon,
+      inline,
+      input,
+      label,
+      meta,
+      type,
+      dirtyOverride = false,
+      ...other
+    } = props;
+    const { error, touched } = meta;
+
     return (
-      <div>
-        <form>
-          <textarea
-            className="large-input"
-            type="text"
-            placeholder="test input"
-            rows="5"
-            cols="80"
-          />
-        </form>
+      <div
+        className={classnames('form-group', className, {
+          'form-group--inline': inline,
+        })}
+      >
+        <textarea
+          {...input}
+          {...other}
+          autoFocus={autoFocus}
+          className={classnames('input', touched && error && 'error')}
+          type={type}
+          onBlur={() => input.onBlur()}
+        />
+
+        {label && (
+          <TextAreaLabel {...meta} dirtyOverride={dirtyOverride}>
+            {label}
+          </TextAreaLabel>
+        )}
+        {touched && error && <TextAreaError>{error}</TextAreaError>}
+        {icon && ICON_SET[icon]}
       </div>
     );
   }

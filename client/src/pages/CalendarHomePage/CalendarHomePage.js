c6bb4a2d2fc8b2549f48914f3fcc7122b691688f
Sean Williams <sean@Prismtechstudios.com>
2019-04-19 16:04:59 -0600

    Added & started styling React Dates calendar on the Show Calendar page

--- client/src/pages/CalendarHomePage/CalendarHomePage.js
+++ client/src/pages/CalendarHomePage/CalendarHomePage.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import {
+  DayPickerSingleDateController,
+  VERTICAL_ORIENTATION,
+} from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 import Alert from '../../components/Alert';
 
@@ -26,16 +32,30 @@ class CalendarHomePage extends Component {
     const { showsError } = this.props;
 
     return (
-      <div className="calendar__view">
-        {showsError && (
-          <Alert
-            type="danger"
-            header="Error Loading Shows"
-            text={showsError.response.data.message}
+      <div className="calendar">
+        <div className="calendar__sidebar">
+          <DayPickerSingleDateController
+            date={moment()}
+            horizontalMonthPadding="0"
+            noBorder={true}
+            daySize="26"
+            numberOfMonths="1"
+            onDateChange={date => console.log(date)}
+            focused={true}
+            onFocusChange={({ focused }) => this.setState({ focused: true })} // Force the focused states to always be truthy so that date is always selectable
+            id="sidebar_calendar"
           />
-        )}
-
-        <ShowCalendar />
+        </div>
+        <div className="calendar__view">
+          {showsError && (
+            <Alert
+              type="danger"
+              header="Error Loading Shows"
+              text={showsError.response.data.message}
+            />
+          )}
+          <ShowCalendar />
+        </div>
       </div>
     );
   }

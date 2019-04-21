ceff4a92a9c6db44fc03daaaf2f042bb5512ac97
Kyle Middleton <kylemid321@gmail.com>
2019-04-21 15:27:34 -0600

    added handleKeyCommand function

--- client/src/components/RichTextArea/RichTextArea.js
+++ client/src/components/RichTextArea/RichTextArea.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 export const ICON_SET = {
   search: <i className="icon fas fa-search" />,
@@ -9,7 +9,7 @@ export const ICON_SET = {
 
 export const RichTextAreaError = props => {
   const { children } = props;
-  return <div className="input__error">{children}</div>;
+  return <div className="richtextarea__error">{children}</div>;
 };
 
 export const RichTextAreaLabel = props => {
@@ -42,6 +42,17 @@ class RichTextArea extends Component {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
     this.onChange = editorState => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command, editorState) {
+    // Allows for keyboard shortcuts for bold and italic text
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
 
   render() {
@@ -73,6 +84,7 @@ class RichTextArea extends Component {
           editorState={this.state.editorState}
           onChange={this.onChange}
           autoFocus={autoFocus}
+          handleKeyCommand={this.handleKeyCommand}
           className={classnames('richtextarea', touched && error && 'error')}
           type={type}
           onBlur={() => input.onBlur()}

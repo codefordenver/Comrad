e3cfec26383fcab447459d360eb6d034e3a8acf8
Kyle Middleton <kylemid321@gmail.com>
2019-04-21 17:06:00 -0600

    revert to development version

--- client/src/pages/UserAddPage/UserAddPage.js
+++ client/src/pages/UserAddPage/UserAddPage.js
@@ -49,71 +49,71 @@ class UserAddPage extends Component {
   render() {
     return (
       <div>Test</div>
-      <Form handleFormSubmit={this.handleFormSubmit}>
-        <Label text="Email" />
-        <Input
-          name="email"
-          onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
-          placeholder="Email"
-          type="text"
-          value={this.state.email}
-        />
-        <Label text="First Name" />
-        <Input
-          name="first_name"
-          onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
-          placeholder="First Name"
-          type="text"
-          value={this.state.first_name}
-        />
-        <Label text="Last Name" />
-        <Input
-          name="last_name"
-          onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
-          placeholder="Last Name"
-          type="text"
-          value={this.state.last_name}
-        />
-        <Label text="On Air Name" />
-        <Input
-          name="on_air_name"
-          onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
-          placeholder="On Air Name"
-          type="text"
-          value={this.state.on_air_name}
-        />
-        <Label text="Role" />
-        <Select
-          name="role"
-          onChange={this.handleInputChange}
-          selectOptions={roleOptions}
-          placeholder="Role"
-          type="text"
-          value={this.state.role}
-        />
-        <Label text="Status" />
-        <Select
-          name="status"
-          onChange={this.handleInputChange}
-          selectOptions={statusOpions}
-          placeholder="Status"
-          type="text"
-          value={this.state.status}
-        />
-        <Label text="Can Delete" />
-        <Checkbox
-          name="can_delete"
-          onClick={this.handleCheckBox}
-          type="checkbox"
-          value={this.state.can_delete}
-        />
-        <button type="submit">Submit</button>
-        <Link to="/user">Go Back</Link>
-      </Form>
+      // <Form handleFormSubmit={this.handleFormSubmit}>
+      //   <Label text="Email" />
+      //   <Input
+      //     name="email"
+      //     onChange={this.handleInputChange}
+      //     onBlur={this.handleInputBlur}
+      //     placeholder="Email"
+      //     type="text"
+      //     value={this.state.email}
+      //   />
+      //   <Label text="First Name" />
+      //   <Input
+      //     name="first_name"
+      //     onChange={this.handleInputChange}
+      //     onBlur={this.handleInputBlur}
+      //     placeholder="First Name"
+      //     type="text"
+      //     value={this.state.first_name}
+      //   />
+      //   <Label text="Last Name" />
+      //   <Input
+      //     name="last_name"
+      //     onChange={this.handleInputChange}
+      //     onBlur={this.handleInputBlur}
+      //     placeholder="Last Name"
+      //     type="text"
+      //     value={this.state.last_name}
+      //   />
+      //   <Label text="On Air Name" />
+      //   <Input
+      //     name="on_air_name"
+      //     onChange={this.handleInputChange}
+      //     onBlur={this.handleInputBlur}
+      //     placeholder="On Air Name"
+      //     type="text"
+      //     value={this.state.on_air_name}
+      //   />
+      //   <Label text="Role" />
+      //   <Select
+      //     name="role"
+      //     onChange={this.handleInputChange}
+      //     selectOptions={roleOptions}
+      //     placeholder="Role"
+      //     type="text"
+      //     value={this.state.role}
+      //   />
+      //   <Label text="Status" />
+      //   <Select
+      //     name="status"
+      //     onChange={this.handleInputChange}
+      //     selectOptions={statusOpions}
+      //     placeholder="Status"
+      //     type="text"
+      //     value={this.state.status}
+      //   />
+      //   <Label text="Can Delete" />
+      //   <Checkbox
+      //     name="can_delete"
+      //     onClick={this.handleCheckBox}
+      //     type="checkbox"
+      //     value={this.state.can_delete}
+      //   />
+      //   <button type="submit">Submit</button>
+      //   <Link to="/user">Go Back</Link>
+      // </Form>
     );
   }
 }

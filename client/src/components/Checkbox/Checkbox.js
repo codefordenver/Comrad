3d05815c00f2e755b38791633550d09d3e94cda5
Kyle Middleton <kylemid321@gmail.com>
2019-04-25 11:27:01 -0600

    revert to development

--- client/src/components/Checkbox/Checkbox.js
+++ client/src/components/Checkbox/Checkbox.js
@@ -1,27 +1,9 @@
-import React, { Component } from 'react';
-import './_Checkbox.scss';
+import React from 'react';
 
-class Checkbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Checkbox = props => {
+  const { className, ...rest } = props;
 
-  render() {
-    const { className, ...rest } = this.props;
-    return (
-      <div className="checkbox-container">
-        <input
-          type="checkbox"
-          name={this.props.label}
-          className={`check-box ${className || ''}`}
-          {...rest}
-        />
-        <span className="checkmark" />
-        <div className="checkbox-label">{this.props.label}</div>
-      </div>
-    );
-  }
-}
+  return <input className={`check-box ${className || ''}`} {...rest} />;
+};
 
 export default Checkbox;

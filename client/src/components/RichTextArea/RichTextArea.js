69f02ff935289ffb0c32a7549cdb45b083096324
Kyle Middleton <kylemid321@gmail.com>
2019-04-25 19:18:35 -0600

    switched to custom toolbar setup

--- client/src/components/RichTextArea/RichTextArea.js
+++ client/src/components/RichTextArea/RichTextArea.js
@@ -2,12 +2,75 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import Editor from 'draft-js-plugins-editor';
 import { EditorState, RichUtils } from 'draft-js';
-import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
 import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+import editorStyles from './editorStyles.css';
 
-const staticToolbarPlugin = createToolbarPlugin();
-const { Toolbar } = staticToolbarPlugin;
-const plugins = [staticToolbarPlugin];
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      window.addEventListener('click', this.onWindowClick);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((
+          Button,
+          i, // eslint-disable-next-line
+        ) => (
+          <Button key={i} {...this.props} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class HeadlinesButton extends Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div className="headlineButtonWrapper">
+        <button onClick={this.onClick} className="headlineButton">
+          H
+        </button>
+      </div>
+    );
+  }
+}
+
+const toolbarPlugin = createToolbarPlugin();
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
 
 export const ICON_SET = {
   search: <i className="icon fas fa-search" />,
@@ -89,7 +152,17 @@ class RichTextArea extends Component {
         })}
       >
         <div className="editor-container" onClick={this.focus}>
-          <Toolbar className="toolbar" />
+          <Toolbar>
+            {// may be use React.Fragment instead of div to improve perfomance after React 16
+            externalProps => (
+              <div>
+                <BoldButton {...externalProps} />
+                <ItalicButton {...externalProps} />
+                <CodeButton {...externalProps} />
+                <HeadlinesButton {...externalProps} />
+              </div>
+            )}
+          </Toolbar>
           <Editor
             {...input}
             {...other}

7fc02f35b738dbe1441da1c37fd73fc3e33847bd
Kyle Middleton <kylemid321@gmail.com>
2019-04-27 11:27:09 -0600

    switched package from draftjs to quill

--- client/src/components/RichTextArea/RichTextArea.js
+++ client/src/components/RichTextArea/RichTextArea.js
@@ -1,75 +1,7 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import Editor from 'draft-js-plugins-editor';
-import { EditorState, RichUtils } from 'draft-js';
-import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
-import 'draft-js-static-toolbar-plugin/lib/plugin.css';
-import {
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  CodeButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-  CodeBlockButton,
-} from 'draft-js-buttons';
-
-class HeadlinesPicker extends Component {
-  componentDidMount() {
-    setTimeout(() => {
-      window.addEventListener('click', this.onWindowClick);
-    });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('click', this.onWindowClick);
-  }
-
-  onWindowClick = () =>
-    // Call `onOverrideContent` again with `undefined`
-    // so the toolbar can show its regular content again.
-    this.props.onOverrideContent(undefined);
-
-  render() {
-    const buttons = [HeadlineTwoButton, HeadlineThreeButton];
-    return (
-      <div>
-        {buttons.map((
-          Button,
-          i, // eslint-disable-next-line
-        ) => (
-          <Button key={i} {...this.props} />
-        ))}
-      </div>
-    );
-  }
-}
-
-class HeadlinesButton extends Component {
-  onClick = () =>
-    // A button can call `onOverrideContent` to replace the content
-    // of the toolbar. This can be useful for displaying sub
-    // menus or requesting additional information from the user.
-    this.props.onOverrideContent(HeadlinesPicker);
-
-  render() {
-    return (
-      <div className="headlineButtonWrapper">
-        <button onClick={this.onClick} className="headlineButton">
-          H
-        </button>
-      </div>
-    );
-  }
-}
-
-const toolbarPlugin = createToolbarPlugin();
-const { Toolbar } = toolbarPlugin;
-const plugins = [toolbarPlugin];
+import ReactQuill, { Quill, Mixin, Toolbar } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const ICON_SET = {
   search: <i className="icon fas fa-search" />,
@@ -109,23 +41,35 @@ export const RichTextAreaLabel = props => {
 class RichTextArea extends Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = editorState => this.setState({ editorState });
-    this.focus = () => {
-      this.editor.focus();
-    };
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.state = { text: '' };
+    //this.handleChange = this.handleChange.bind(this);
   }
 
-  handleKeyCommand(command, editorState) {
-    // Allows for keyboard shortcuts for bold and italic text
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
+  handleChange(value) {
+    this.setState({ text: value });
   }
+
+  modules = {
+    toolbar: [
+      [{ header: [2, 3, false] }],
+      ['bold', 'italic', 'underline'],
+      ['link'],
+    ],
+  };
+
+  formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+  ];
 
   render() {
     const { props } = this;
@@ -151,31 +95,20 @@ class RichTextArea extends Component {
         })}
       >
         <div className="editor-container" onClick={this.focus}>
-          <Toolbar>
-            {// may be use React.Fragment instead of div to improve perfomance after React 16
-            externalProps => (
-              <div>
-                <BoldButton {...externalProps} />
-                <ItalicButton {...externalProps} />
-                <CodeButton {...externalProps} />
-                <HeadlinesButton {...externalProps} />
-              </div>
-            )}
-          </Toolbar>
-          <Editor
+          <ReactQuill
+            //value={this.state.text}
             {...input}
             {...other}
-            editorState={this.state.editorState}
-            onChange={this.onChange}
+            //onChange={this.handleChange}
             autoFocus={autoFocus}
-            handleKeyCommand={this.handleKeyCommand}
-            plugins={plugins}
             ref={element => {
               this.editor = element;
             }}
             className={classnames('richtextarea', touched && error && 'error')}
             type={type}
             onBlur={() => input.onBlur()}
+            modules={this.modules}
+            formats={this.formats}
           />
 
           {label && (

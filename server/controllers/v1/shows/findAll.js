1c9c595707823d7281361efb8aecbf8145b1202d
Sean Williams <sean@Prismtechstudios.com>
2019-04-29 14:04:34 -0600

    Add paging the shows > findAll API

--- server/controllers/v1/shows/findAll.js
+++ server/controllers/v1/shows/findAll.js
@@ -1,7 +1,12 @@
 const db = require('../../../models/v1');
+const keys = require('../../../config/keys');
 
 function findAll(req, res) {
-  db.Show.find({})
+  const { page = 1 } = req.query;
+  db.Show.find({}, null, {
+    skip: (page - 1) * keys.queryPageSize,
+    limit: keys.queryPageSize,
+  })
     .then(dbShow => res.status(200).json(dbShow))
     .catch(err => res.status(422).json(err));
 }

2d8b2eb0676f2f3ccdffb0fca4531575aa19e236
Kyle Middleton <kylemid321@gmail.com>
2019-05-11 14:51:34 -0600

    calculate and pass in maxtracknumber value

--- client/src/pages/TrackAddPage/TrackAddPage.js
+++ client/src/pages/TrackAddPage/TrackAddPage.js
@@ -12,6 +12,7 @@ class TrackAddPage extends Component {
 
   render() {
     const { name, tracks } = this.props.album.doc;
+    // this function calculates the maximum disk number
     let array = [];
     for (var key in tracks) {
       array.push(tracks[key].disk_number);
@@ -19,14 +20,30 @@ class TrackAddPage extends Component {
     var maxDiskNumber = array.reduce(function(a, b) {
       return Math.max(a, b);
     });
+    //
 
+    // this calculates the maximum track number
+    let array2 = [];
+    for (var key in tracks) {
+      if (tracks[key].disk_number === maxDiskNumber) {
+        array2.push(tracks[key].track_number);
+      }
+    }
+
+    var maxTrackNumber = array2.reduce(function(a, b) {
+      return Math.max(a, b);
+    });
+    //
     return (
       <div>
         <Card>
           <CardBody>
             <h1>Add Track to Album</h1>
             <h2>Album: {name}</h2>
-            <FormTrackAdd maxDiskNumber={maxDiskNumber} />
+            <FormTrackAdd
+              maxDiskNumber={maxDiskNumber}
+              maxTrackNumber={maxTrackNumber}
+            />
           </CardBody>
         </Card>
       </div>

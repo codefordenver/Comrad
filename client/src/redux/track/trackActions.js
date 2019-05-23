17037d192aee95a8c952ac428af654396abbfd7b
Kyle Middleton <kylemid321@gmail.com>
2019-05-22 20:25:49 -0600

    remove unneeded code

--- client/src/redux/track/trackActions.js
+++ client/src/redux/track/trackActions.js
@@ -3,7 +3,7 @@ import { TRACK_ADD } from './trackTypes';
 
 export const trackAdd = (input, callback, id) => async dispatch => {
   try {
-    const response = await axios.post(`/v1/tracks`, input); //correct path?
+    const response = await axios.post(`/v1/tracks`, input);
     dispatch({ type: TRACK_ADD, payload: response.data });
     callback(response.data._id);
   } catch (e) {

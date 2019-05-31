0711e4fe13e07c89688aece10d14326bbb83e1d0
Kyle Middleton <kylemid321@gmail.com>
2019-05-31 13:42:50 -0600

    initial import of validateTrackData (commented out)

--- server/v1/controllers/tracks/create.js
+++ server/v1/controllers/tracks/create.js
@@ -1,6 +1,11 @@
 const db = require('../../models');
+const { validateTrackData } = require('./utils');
 
 function create(req, res) {
+  //validateTrackData(req.body, req.params.albumId)
+  //  .then(req => db.Track.create(req.body))
+  //  .then(dbTrack => res.json(dbTrack))
+  //  .catch(err => res.status(422).json(err));
   db.Track.create(req.body)
     .then(dbTrack => res.json(dbTrack))
     .catch(err => res.status(422).json(err));

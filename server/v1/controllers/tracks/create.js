2f352b262cc48c18547ec4251acdc92cc3c0ff1d
Kyle Middleton <kylemid321@gmail.com>
2019-06-14 15:25:25 -0600

    implement validation

--- server/v1/controllers/tracks/create.js
+++ server/v1/controllers/tracks/create.js
@@ -2,13 +2,13 @@ const db = require('../../models');
 const { validateTrackData } = require('./utils');
 
 function create(req, res) {
-  //validateTrackData(req.body, req.params.albumId)
-  //  .then(req => db.Track.create(req.body))
-  //  .then(dbTrack => res.json(dbTrack))
-  //  .catch(err => res.status(422).json(err));
-  db.Track.create(req.body)
+  validateTrackData(req.body)
+    .then(() => db.Track.create(req.body))
     .then(dbTrack => res.json(dbTrack))
-    .catch(err => res.status(422).json(err));
+    .catch(err => {
+      console.log(err);
+      res.status(422).json(err);
+    });
 }
 
 module.exports = create;

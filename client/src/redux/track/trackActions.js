7232b475a53614effbbd4e0079862c071576c62c
Kyle Middleton <kylemid321@gmail.com>
2019-06-14 21:54:26 -0600

    change response.data._id to response.data in callback

--- client/src/redux/track/trackActions.js
+++ client/src/redux/track/trackActions.js
@@ -5,7 +5,7 @@ export const trackAdd = (input, callback, id) => async dispatch => {
   try {
     const response = await axios.post(`/v1/tracks`, input);
     dispatch({ type: TRACK_ADD, payload: response.data });
-    callback(response.data._id);
+    callback(response.data);
   } catch (e) {
     console.error(e);
   }

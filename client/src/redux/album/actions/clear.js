d7ed65048f9730644c9073ce6f4d33c7f60327a5
Kyle Middleton <kylemid321@gmail.com>
2019-06-17 21:45:19 -0600

    remove unneeded console.log

--- client/src/redux/album/actions/clear.js
+++ client/src/redux/album/actions/clear.js
@@ -2,7 +2,6 @@ import { albumTypes } from '../albumTypes';
 
 export const clear = () => async dispatch => {
   try {
-    console.log('clear');
     dispatch({ type: albumTypes.CLEAR });
   } catch (err) {
     console.log(err);

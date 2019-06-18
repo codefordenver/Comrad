b6bbf49affadc9295d6fec7efdc4c767ae6c65e4
Kyle Middleton <kylemid321@gmail.com>
2019-06-17 21:31:06 -0600

    remove unneeded parameter

--- client/src/redux/track/trackActions.js
+++ client/src/redux/track/trackActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { TRACK_ADD } from './trackTypes';
 
-export const trackAdd = (input, callback, id) => async dispatch => {
+export const trackAdd = (input, callback) => async dispatch => {
   try {
     const response = await axios.post(`/v1/tracks`, input);
     dispatch({ type: TRACK_ADD, payload: response.data });

58c0932f9b93cd1a56fa7a41eae6bd8e6dfee72d
dzimmerman902 <dzimmerman902@gmail.com>
2019-06-24 18:53:03 -0600

    update(albumController): Updated syntax

--- server/v1/controllers/albums/create.js
+++ server/v1/controllers/albums/create.js
@@ -2,7 +2,7 @@ const db = require('../../models');
 
 function create(req, res) {
   db.Album.create(req.body)
-    .then(dbAnnouncement => res.json(dbAnnouncement))
+    .then(dbAlbum => res.json(dbAlbum))
     .catch(err => res.status(422).json(err));
 }
 

93759bf7bb80b778532618ee2078fabe215d2ebc
Kyle Middleton <kylemid321@gmail.com>
2019-06-29 20:50:04 -0600

    implement validateEditData

--- server/v1/controllers/tracks/update.js
+++ server/v1/controllers/tracks/update.js
@@ -1,7 +1,13 @@
 const db = require('../../models');
+const { validateEditData } = require('./utils');
 
 function update(req, res) {
-  db.Track.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+  validateEditData(req.body)
+    .then(() =>
+      db.Track.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+      }),
+    )
     .then(dbTrack => res.json(dbTrack))
     .catch(err => res.status(422).json(err));
 }

14ef1c0596c535abcd1b2d2d19cb52cb8c46da15
dzimmerman902 <dzimmerman902@gmail.com>
2019-07-05 13:58:33 -0600

    new(ResourcePages): Created 3 new pages for the resources section

---
+++ client/src/pages/ResourcePoliciesPage/ResourcePoliciesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ResourcePoliciesPage extends Component {
+  render() {
+    return (
+      <div className="rpolp">
+        <h3>Policies</h3>
+      </div>
+    );
+  }
+}
+
+export default ResourcePoliciesPage;
---
+++ client/src/pages/ResourceStationRulesPage/ResourceStationRulesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ResourceStationRulesPage extends Component {
+  render() {
+    return (
+      <div className="rsrp">
+        <h3>Station Rules</h3>
+      </div>
+    );
+  }
+}
+
+export default ResourceStationRulesPage;

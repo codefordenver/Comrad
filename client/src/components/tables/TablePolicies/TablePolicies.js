5b98edadcd21499f7564f2f36711bb0db3494f79
dzimmerman902 <dzimmerman902@gmail.com>
2019-07-06 13:11:14 -0600

    new(Policies): Created basic page and components for policies

--- client/src/components/tables/TablePolicies/TablePolicies.js
+++ client/src/components/tables/TablePolicies/TablePolicies.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ReactTable from 'react-table';
+import { bindActionCreators } from 'redux';
+
+import { resourceActions } from '../../../redux/resource';
 
 const columns = [
   {
-    Header: 'Name',
-    value: 'name',
+    Header: 'Description',
+    accessor: 'description',
   },
   {
     Header: 'Link',
-    value: 'link',
+    accessor: 'link',
   },
 ];
 
 class TablePolicies extends Component {
+  componentDidMount() {
+    const { resourceActions } = this.props;
+    resourceActions.findAll();
+  }
+
   render() {
-    return <ReactTable className="-highlight" columns={columns} />;
+    const { resourceState } = this.props;
+
+    return (
+      <ReactTable
+        className="-highlight"
+        columns={columns}
+        data={resourceState.docs}
+        minRows={3}
+        showPagination={false}
+      />
+    );
   }
 }
 
-export default TablePolicies;
+function mapStateToProps({ resource }) {
+  return {
+    resourceState: resource,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    resourceActions: bindActionCreators({ ...resourceActions }, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TablePolicies);
--- client/src/pages/ResourcePoliciesPage/ResourcePoliciesPage.js
+++ client/src/pages/ResourcePoliciesPage/ResourcePoliciesPage.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 
+import Button from '../../components/Button';
 import TablePolicies from '../../components/tables/TablePolicies';
 
 class ResourcePoliciesPage extends Component {
+  state = {
+    content: 'table',
+  };
+
+  handleSwitch = () => {
+    this.setState({
+      content: 'form',
+    });
+  };
+
+  renderHeader = () => {
+    const { handleSwitch, state } = this;
+
+    switch (state.content) {
+      case 'table':
+        return (
+          <>
+            <h3>Policies</h3>
+            <Button onClick={handleSwitch}>Add</Button>
+          </>
+        );
+      case 'form':
+        return <h3>Add Policy</h3>;
+      default:
+        break;
+    }
+  };
+
+  renderContent = () => {
+    const { state } = this;
+
+    switch (state.content) {
+      case 'table':
+        return <TablePolicies />;
+      case 'form':
+        return <div>Form</div>;
+      default:
+        break;
+    }
+  };
+
   render() {
+    const { renderContent, renderHeader } = this;
+
     return (
       <div className="rpolp">
-        <h3>Policies</h3>
-        <TablePolicies />
+        <div className="rpolp__header">{renderHeader()}</div>
+        <div className="rpolp__content">{renderContent()}</div>
       </div>
     );
   }

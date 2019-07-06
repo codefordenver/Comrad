5b1e0831fe8e738314112fa186286b17fa09e610
Kyle Middleton <kylemid321@gmail.com>
2019-07-06 16:24:23 -0600

    add add action to reducers

--- client/src/redux/album/albumReducers.js
+++ client/src/redux/album/albumReducers.js
@@ -48,6 +48,13 @@ export const albumReducer = (state = initialState, { type, payload }) => {
         ...state,
         doc: [],
       };
+    case albumTypes.ADD:
+      return {
+        ...state,
+        doc: {
+          ...payload,
+        },
+      };
     default:
       return state;
   }

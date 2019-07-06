ad8d0c30e3e20036543dc6e626341ce50ba78d31
Kyle Middleton <kylemid321@gmail.com>
2019-07-06 16:18:56 -0600

    import add to albumAPI object

--- client/src/api/album/index.js
+++ client/src/api/album/index.js
@@ -1,8 +1,10 @@
 import { remove } from './remove';
 import { findOne } from './findOne';
 import { findOneTracks } from './findOneTracks';
+import { add } from './add';
 
 export const albumAPI = {
+  add,
   remove,
   findOne,
   findOneTracks,

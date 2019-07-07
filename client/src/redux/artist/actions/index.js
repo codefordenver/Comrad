83cf9a77bcbd99ad4935898a70656017e781ee7c
Kyle Middleton <kylemid321@gmail.com>
2019-07-06 19:25:13 -0600

    import and implement clear action

--- client/src/redux/artist/actions/index.js
+++ client/src/redux/artist/actions/index.js
@@ -1,6 +1,7 @@
 import { add } from './add';
 import { alertClose } from './alertClose';
 import { changeEditingArtistName } from './changeEditingArtistName';
+import { clear } from './clear';
 import { findAlbums } from './findAlbums';
 import { findOne } from './findOne';
 import { remove } from './remove';
@@ -10,6 +11,7 @@ export const artistActions = {
   add,
   alertClose,
   changeEditingArtistName,
+  clear,
   findAlbums,
   findOne,
   remove,

b83fdbb4ace8e641d4007b90e8874493d4c65869
Kyle Middleton <kylemid321@gmail.com>
2019-07-06 18:09:01 -0600

    add add to albumActions object

--- client/src/redux/album/actions/index.js
+++ client/src/redux/album/actions/index.js
@@ -2,9 +2,11 @@ import { alertClose } from './alertClose';
 import { clear } from './clear';
 import { findOne } from './findOne';
 import { remove } from './remove';
+import { add } from './add';
 
 export const albumActions = {
   alertClose,
+  add,
   clear,
   findOne,
   remove,

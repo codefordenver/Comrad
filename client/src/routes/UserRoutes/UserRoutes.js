cf19346d2b6eacc44e80f2a8338c61eb3dcef3dd
Kyle Middleton <kylemid321@gmail.com>
2019-07-08 20:22:09 -0600

    add route for changepasswordpage

--- client/src/routes/UserRoutes/UserRoutes.js
+++ client/src/routes/UserRoutes/UserRoutes.js
@@ -6,17 +6,22 @@ import UserAddPage from '../../pages/UserAddPage';
 import UserEditPage from '../../pages/UserEditPage';
 import UserSearchPage from '../../pages/UserSearchPage';
 import UserProfilePage from '../../pages/UserProfilePage';
+import ChangePasswordPage from '../../pages/ChangePasswordPage/ChangePasswordPage';
 
 class UserRoutes extends Component {
   render() {
     const { url } = this.props.match;
-
     return (
       <MainLayout {...this.props}>
         <Route exact path={`${url}/add`} component={UserAddPage} />
         <Route exact path={`${url}/edit`} component={UserEditPage} />
         <Route exact path={`${url}/search`} component={UserSearchPage} />
         <Route exact path={`${url}/profile/:id`} component={UserProfilePage} />
+        <Route
+          exact
+          path={`${url}/profile/change-password`}
+          component={ChangePasswordPage}
+        />
       </MainLayout>
     );
   }

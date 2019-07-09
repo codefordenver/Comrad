2c170dd46258458e86a136e7f936f32c8969e805
Kyle Middleton <kylemid321@gmail.com>
2019-07-08 22:26:03 -0600

    add user to path in change password link

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
           <div className="navbar__user">
             <Dropdown position="bottom-left" type="icon">
               <Dropdown.Item to="/profile/edit">Edit Profile</Dropdown.Item>
-              <Dropdown.Item to="/profile/change-password">
+              <Dropdown.Item to="/user/profile/change-password">
                 Change Password
               </Dropdown.Item>
               <Dropdown.Item handleOnClick={this.handleSignOut}>

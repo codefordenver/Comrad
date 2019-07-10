030e813d1d32e1a8ea03a74526313e1f6ef597b0
Kyle Middleton <kylemid321@gmail.com>
2019-07-09 20:19:45 -0600

    initial commit

---
+++ server/v1/controllers/auth/passwordChange.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt-nodejs');
+const db = require('../../models');
+
+async function passwordChange(req, res) {
+  const { passConfirm, passNew, _id } = req.body;
+
+  if (!passConfirm || !passNew) {
+    return res.status(401).json('Password Confirm and/or Password New Missing');
+  }
+
+  if (passConfirm !== passNew) {
+    return res.status(401).json('Your passwords do not match!');
+  }
+
+  const user = await db.User.findOne({
+    id: _id,
+  });
+
+  if (!user) {
+    return res.status(404).json('Your token has expired or is invalid');
+  }
+
+  await bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return res.status(422).json(err);
+    }
+
+    bcrypt.hash(passNew, salt, null, async (err, hash) => {
+      if (err) {
+        return res.status(422).json(err);
+      }
+
+      const updatedUser = await db.User.findOneAndUpdate(
+        { email: user.email },
+        {
+          password: hash,
+          reset_token: null,
+          reset_token_expiry: null,
+        },
+        { new: true },
+      );
+
+      return res.json(updatedUser);
+    });
+  });
+}
+
+module.exports = passwordChange;

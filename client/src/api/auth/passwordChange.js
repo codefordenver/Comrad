59e667b6099928f148cf160d377756d6b4a82a2e
Kyle Middleton <kylemid321@gmail.com>
2019-07-09 21:47:52 -0600

    pass in id

--- client/src/api/auth/passwordChange.js
+++ client/src/api/auth/passwordChange.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { ROOT_AUTH_URL } from '../root';
 
-export function passwordChange({ passConfirm, passNew }) {
-  return axios.put(`${ROOT_AUTH_URL}/password/new`, {
+export function passwordChange({ passConfirm, passNew, id }) {
+  return axios.put(`${ROOT_AUTH_URL}/password/change`, {
     passConfirm,
     passNew,
+    id,
   });
 }

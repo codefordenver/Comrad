d23a02dfd48685188c8ae8221b21fa0f0e424303
Kyle Middleton <kylemid321@gmail.com>
2019-07-09 18:19:44 -0600

    initial commit

---
+++ client/src/redux/auth/actions/passwordChange.js
@@ -0,0 +1,31 @@
+import { authAPI } from '../../../api';
+import { alertTypes } from '../../alert';
+import { authTypes } from '../authTypes';
+
+export const passwordChange = (values, callback) => async dispatch => {
+  try {
+    dispatch({ type: authTypes.LOADING });
+
+    await authAPI.passwordChange(values);
+
+    callback();
+
+    dispatch({
+      type: alertTypes.ACTIVE,
+      payload: {
+        header: 'Success',
+        body: 'Your Password Has Been Successfully Resetted',
+        type: 'success',
+      },
+    });
+  } catch (err) {
+    dispatch({
+      type: alertTypes.ACTIVE,
+      payload: {
+        header: 'Error',
+        body: 'Error resetting password',
+        type: 'danger',
+      },
+    });
+  }
+};

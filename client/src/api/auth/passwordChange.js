33dc3879bf7077042c1d7601b1fcdf0d64396fd6
Kyle Middleton <kylemid321@gmail.com>
2019-07-10 20:46:56 -0600

    replace id with _id

--- client/src/api/auth/passwordChange.js
+++ client/src/api/auth/passwordChange.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { ROOT_AUTH_URL } from '../root';
 
-export function passwordChange({ passConfirm, passNew, id }) {
+export function passwordChange({ passConfirm, passNew, _id }) {
   return axios.put(`${ROOT_AUTH_URL}/password/change`, {
     passConfirm,
     passNew,
-    id,
+    _id,
   });
 }

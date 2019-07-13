1fb33076f22b18417f2914429f56c362ed80de5c
Kyle Middleton <kylemid321@gmail.com>
2019-07-12 18:45:05 -0600

    remove passConfirm param

--- client/src/api/auth/passwordChange.js
+++ client/src/api/auth/passwordChange.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import { ROOT_AUTH_URL } from '../root';
 
-export function passwordChange({ passConfirm, passNew, _id }) {
+export function passwordChange({ passNew, _id }) {
   return axios.put(`${ROOT_AUTH_URL}/password/change`, {
-    passConfirm,
     passNew,
     _id,
   });

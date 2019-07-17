6075287a3d4fe20a905d411c7d78a356d1bd5988
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2019-07-16 22:23:58 -0700

    moves delete modal over to table album tracks

--- client/src/components/tables/TableAlbumTracks/TableAlbumTracks.js
+++ client/src/components/tables/TableAlbumTracks/TableAlbumTracks.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import ReactTable from 'react-table';
 import isEmpty from 'lodash/isEmpty';
 
 import { formatTotalSecondsAsMMSS } from '../../../utils/formatters';
 import Dropdown from '../../Dropdown';
+import Modal from '../../Modal';
+import Button from '../../Button';
+
+import { trackActions, alertActions } from '../../../redux';
 
 const CellDuration = ({ value }) =>
   value ? (
@@ -14,6 +19,42 @@ const CellDuration = ({ value }) =>
   ) : null;
 
 class TableAlbumTracks extends Component {
+  state = {
+    deleteModal: false,
+    deleteSuccessModal: false,
+  };
+
+  closeDeleteModal = () => {
+    this.setState({ deleteModal: false });
+  };
+
+  closeDeleteSuccessModal = () => {
+    this.setState({ deleteSuccessModal: false });
+  };
+
+  deleteTrack = id => {
+    const { trackActions } = this.props;
+    trackActions.remove(id, this.deleteSuccess, this.deleteFailure);
+  };
+
+  deleteFailure = () => {
+    this.closeDeleteModal();
+  };
+
+  deleteSuccess = entity => {
+    this.closeDeleteModal();
+    this.props.alertActions.hide();
+    this.setState(
+      {
+        deleteSuccessModal: entity.data,
+      },
+      function() {
+        // console.log('table :', this.table);
+        // this.table.fireFetchData();
+      },
+    );
+  };
+
   handleRowClick = (state, rowInfo) => {
     if (!isEmpty(rowInfo)) {
       return {
@@ -37,9 +78,9 @@ class TableAlbumTracks extends Component {
   handleRowDeleteClick = data => {
     console.log('delete was clicked');
     console.log(data);
-    // this.setState({
-    //   deleteModal: data,
-    // });
+    this.setState({
+      deleteModal: data,
+    });
   };
 
   stopPropagation = event => {
@@ -47,7 +88,8 @@ class TableAlbumTracks extends Component {
   };
 
   render() {
-    const { handleRowClick, props } = this;
+    const { handleRowClick, props, state } = this;
+    const { deleteModal, deleteSuccessModal } = state;
     const { albumState } = props;
     const { tracks } = albumState.doc;
 
@@ -105,16 +147,53 @@ class TableAlbumTracks extends Component {
     ];
 
     return (
-      <ReactTable
-        className="-highlight table-album-tracks clickable-rows"
-        columns={columns}
-        data={tracks}
-        showPageSizeOptions={false}
-        defaultPageSize={100}
-        minRows={3} // so the formatting does not look weird when there are 0 records
-        noDataText="This album does not have any tracks"
-        getTdProps={handleRowClick}
-      />
+      <div>
+        <ReactTable
+          className="-highlight table-album-tracks clickable-rows"
+          columns={columns}
+          data={tracks}
+          showPageSizeOptions={false}
+          defaultPageSize={100}
+          minRows={3} // so the formatting does not look weird when there are 0 records
+          noDataText="This album does not have any tracks"
+          getTdProps={handleRowClick}
+        />
+
+        {/* Delete modal */}
+        {deleteModal ? (
+          <Modal isOpen={true}>
+            <div className="library-search__delete-modal">
+              Are you sure you want to delete the {deleteModal.type}{' '}
+              <i>{deleteModal.name}</i>?
+              <div>
+                <Button color="neutral" onClick={this.closeDeleteModal}>
+                  No
+                </Button>
+                <Button
+                  type="submit"
+                  onClick={() => this.deleteTrack(deleteModal._original._id)}
+                >
+                  Yes
+                </Button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+
+        {/* Delete confirmation modal */}
+        {deleteSuccessModal ? (
+          <Modal isOpen={true}>
+            <div className="library-search__delete-success-modal">
+              <i>{deleteSuccessModal.name}</i> was successfully deleted.
+              <div>
+                <Button color="neutral" onClick={this.closeDeleteSuccessModal}>
+                  Close
+                </Button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+      </div>
     );
   }
 }
@@ -125,7 +204,14 @@ function mapStateToProps({ album }) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    alertActions: bindActionCreators({ ...alertActions }, dispatch),
+    trackActions: bindActionCreators({ ...trackActions }, dispatch),
+  };
+}
+
 export default connect(
   mapStateToProps,
-  null,
+  mapDispatchToProps,
 )(TableAlbumTracks);

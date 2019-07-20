6283e5d274fc2ffd8d02992d43215dac1b4f18e4
Kyle Middleton <kylemid321@gmail.com>
2019-07-20 10:44:36 -0600

    remove dispatch

--- client/src/redux/genre/actions/findAll.js
+++ client/src/redux/genre/actions/findAll.js
@@ -3,8 +3,6 @@ import { genreAPI } from '../../../api';
 
 export const findAll = () => async dispatch => {
   try {
-    dispatch({ type: genreTypes.FIND_ALL });
-
     const { data: docs } = await genreAPI.findAll();
 
     dispatch({ type: genreTypes.FIND_ALL, payload: docs });

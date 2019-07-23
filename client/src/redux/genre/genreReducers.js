ef089db367f1d2a55a87eadaf2c264a0f8ec5dc0
Kyle Middleton <kylemid321@gmail.com>
2019-07-23 17:35:40 -0600

    remove ...state from FIND_ALL type

--- client/src/redux/genre/genreReducers.js
+++ client/src/redux/genre/genreReducers.js
@@ -1,20 +1,16 @@
 import { genreTypes } from './genreTypes';
 
 const initialState = {
-  doc: {},
   docs: [],
-  loading: false,
-  search: {
-    filter: 'all',
-    q: '',
-  },
 };
 
 export const genreReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case genreTypes.FIND_ALL:
       return {
-        ...state,
+        docs: {
+          ...payload,
+        },
       };
 
     default:

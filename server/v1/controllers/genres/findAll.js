5c41b3ba6d924ef50a3d7005f5c0383d5a6f347b
Kyle Middleton <kylemid321@gmail.com>
2019-07-24 14:11:23 -0600

    return genre names only

--- server/v1/controllers/genres/findAll.js
+++ server/v1/controllers/genres/findAll.js
@@ -1,7 +1,13 @@
 const db = require('../../models');
 
 function findAll(req, res) {
-  return db.Genre.find().then(genres => res.json(genres));
+  return db.Genre.find().then(genres => {
+    let array = [];
+    for (let i = 0; i < genres.length; i++) {
+      array.push(genres[i].name);
+    }
+    res.json(array);
+  });
 }
 
 module.exports = findAll;

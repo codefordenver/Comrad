78219916937d2bc22a0c0c4a4b42388e6e8f0714
Sean Williams <sean@Prismtechstudios.com>
2019-07-28 21:19:13 -0600

    Add missing data to playlist data import

--- server/v1/controllers/playlists/utils/populateTrafficInstance.js
+++ server/v1/controllers/playlists/utils/populateTrafficInstance.js
@@ -3,17 +3,27 @@ const {
 } = require('../../shows/utils');
 
 function populateTrafficInstance(
-  trafficId,
+  trafficObj,
   masterTimeId,
   startTimeUtc,
   endTimeUtc,
 ) {
   let instances = allShowInstancesInDateRange(
-    trafficId,
+    trafficObj,
     startTimeUtc,
     endTimeUtc,
   );
-  return instances.find(a => a.master_time_id === masterTimeId);
+  let instance = instances.find(a => a.master_time_id === masterTimeId);
+
+  // with legacy data from the old version of comrad, sometimes the time of the traffic event has changed from
+  // the master_time_id that was recorded in the database. In these cases, use the first traffic event found
+  // this is not foolproof, but will work in the vast majority of cases. (It won't in cases where a
+  // traffic event has multiple instances within the same show)
+  if (typeof instance === 'undefined') {
+    instance = instances[0];
+  }
+
+  return instance;
 }
 
 module.exports = populateTrafficInstance;

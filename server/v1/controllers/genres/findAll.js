27d811ee5ff0e432a74be38f137afe6ca7eb1864
Kyle Middleton <kylemid321@gmail.com>
2019-08-05 16:29:03 -0600

    return all genre data instead of just names

--- server/v1/controllers/genres/findAll.js
+++ server/v1/controllers/genres/findAll.js
@@ -2,11 +2,7 @@ const db = require('../../models');
 
 function findAll(req, res) {
   return db.Genre.find().then(genres => {
-    let array = [];
-    for (let i = 0; i < genres.length; i++) {
-      array.push(genres[i].name);
-    }
-    res.json(array);
+    res.json(genres);
   });
 }
 

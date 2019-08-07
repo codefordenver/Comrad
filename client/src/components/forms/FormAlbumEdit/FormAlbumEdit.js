ea0d8bddb9bd97f42f98c31f3d9298b1168cc8c6
Sean Williams <sean@Prismtechstudios.com>
2019-08-07 12:49:30 -0600

    Fix for genre undefined issues

--- client/src/components/forms/FormAlbumEdit/FormAlbumEdit.js
+++ client/src/components/forms/FormAlbumEdit/FormAlbumEdit.js
@@ -84,7 +84,7 @@ function mapStateToProps(state) {
       compilation: compilation,
       id: _id,
       custom: custom,
-      genre: genre._id || null,
+      genre: typeof genre !== 'undefined' ? genre._id : null,
     },
   };
 }

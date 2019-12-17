8a13b65a7c2df922829a51e13ab6874b91901403
Sean Williams <sean@Prismtechstudios.com>
2019-12-17 06:29:28 -0700

    Removed extra logging messages

--- server/v1/controllers/playlists/utils/populatePlaylist.js
+++ server/v1/controllers/playlists/utils/populatePlaylist.js
@@ -22,17 +22,14 @@ async function populatePlaylist(docPlaylist) {
 
   let objPlaylist = docPlaylist.toObject();
   for (var i = 0; i < docPlaylist.saved_items.length; i++) {
-    console.log(i);
     let docSavedItems = docPlaylist.saved_items[i];
     if (typeof docSavedItems.traffic !== 'undefined') {
-      console.log('traffic');
       objPlaylist.saved_items[i].traffic = populateTrafficInstance(
         docSavedItems.traffic,
         docSavedItems.master_time_id,
         docPlaylist.start_time_utc,
         docPlaylist.end_time_utc,
       );
-      console.log(objPlaylist.saved_items[i].traffic);
     }
   }
 

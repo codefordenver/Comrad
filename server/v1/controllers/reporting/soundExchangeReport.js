393a3ddf52ca76a808d7e957686fff4fae91429f
Sean Williams <sean@Prismtechstudios.com>
2020-04-14 16:58:13 -0500

    SoundExchange revisions

--- server/v1/controllers/reporting/soundExchangeReport.js
+++ server/v1/controllers/reporting/soundExchangeReport.js
@@ -7,22 +7,28 @@ const { Parser } = require('json2csv');
 // guidelines for generating this file are at
 //https://digitalservices.npr.org/playlist-log-file-guidelines?utm_source=&utm_medium=email&utm_content=20190625&utm_campaign=&utm_term=
 
+// start time for tracks may be more accurate using the executed_time_utc property since that is when the DJ moved the item to Saved Items
+// but, for consistency with the old Comrad system, we will have the first track of the playlist start at the beginning of the show,
+// and then it will "stack" tracks so that the next track is listed as starting as soon as the other track ends
+// the commit at fcfaf689 used a method with executed_time_utc if we ever need to retrieve this
+
 function soundExchangeReport(req, res) {
   let { from, to } = req.query;
 
   let match_query = { 'saved_items.track': { $ne: null } };
   if (from != null && to != null) {
-    match_query['saved_items.executed_time_utc'] = {
+    match_query.start_time_utc = {
       $gte: new Date(from),
       $lte: new Date(to),
     };
   } else if (from != null) {
-    match_query['saved_items.executed_time_utc'] = { $gte: new Date(from) };
+    match_query.start_time_utc = { $gte: new Date(from) };
   } else if (to != null) {
-    match_query['saved_items.executed_time_utc'] = { $lte: new Date(to) };
+    match_query.start_time_utc = { $lte: new Date(to) };
   }
 
   db.Playlist.aggregate([
+    { $sort: { start_time_utc: 1 } },
     { $unwind: { path: '$saved_items' } },
     { $match: match_query },
     {
@@ -35,12 +41,7 @@ function soundExchangeReport(req, res) {
     },
     {
       $project: {
-        executed_time_utc: {
-          $max: [
-            '$saved_items.executed_time_utc',
-            '$saved_items.start_time_utc',
-          ],
-        },
+        time_utc: '$start_time_utc',
         track_info: { $arrayElemAt: ['$track_info', 0] },
       },
     },
@@ -64,15 +65,10 @@ function soundExchangeReport(req, res) {
     },
     {
       $project: {
-        executed_time_utc: 1,
+        time_utc: 1,
         track_info: 1,
         album_info: { $arrayElemAt: ['$album_info', 0] },
         artists: '$artists.name',
-      },
-    },
-    {
-      $sort: {
-        executed_time_utc: 1,
       },
     },
   ])
@@ -90,12 +86,33 @@ function soundExchangeReport(req, res) {
       }
 
       const parser = new Parser({ delimiter: '\t' });
+      var playlistCounter, playlistStartTime;
       const csv = parser.parse(
         soundExchangeData.map(t => {
+          // see explanation at the top of this file for how we are calculating start time
+          console.log('start');
+          console.log(playlistStartTime);
+          console.log(typeof t.time_utc);
+          console.log(t.time_utc);
+          console.log(typeof playlistStartTime);
+          console.log(playlistCounter);
+          if (String(playlistStartTime) !== String(t.time_utc)) {
+            console.log('reset');
+            playlistStartTime = t.time_utc;
+            playlistCounter = 0;
+          }
+          console.log(playlistStartTime);
+          console.log(playlistCounter);
+
+          let startTime = moment(t.time_utc);
+          startTime.add(playlistCounter, 'seconds');
+
+          playlistCounter += t.track_info.duration_in_seconds;
+
+          console.log(playlistCounter);
+
           return {
-            'Start Time': moment(t.executed_time_utc)
-              .tz(keys.stationTimeZone)
-              .format('YYYY-MM-DD HH:mm:ss'),
+            'Start Time': startTime.format('YYYY-MM-DD HH:mm:ss'),
             'End Time': null, //not required, since duration is provided
             Duration: t.track_info.duration_in_seconds,
             Title: t.track_info.name,

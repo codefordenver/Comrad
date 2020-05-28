ec25ac8ad97bea04e8fda2022f1758541aa9905a
Sean Williams <sean@prismtechstudios.com>
2020-05-28 12:46:04 +0000

    Changes for deploying API to production

--- client/src/api/root.js
+++ client/src/api/root.js
@@ -1,11 +1,20 @@
 const VERSION = 'v1';
 
-export const ROOT_AUTH_URL = `/${VERSION}/auth`;
-export const ROOT_CONFIG_URL = `/${VERSION}/config`;
-export const ROOT_GENRE_URL = `/${VERSION}/genres`;
-export const ROOT_LIBRARY_URL = `/${VERSION}/library`;
-export const ROOT_PLAYLISTS_URL = `/${VERSION}/playlists`;
-export const ROOT_RESOURCES_URL = `/${VERSION}/resources`;
-export const ROOT_SHOWS_URL = `/${VERSION}/events/shows`;
-export const ROOT_TRAFFIC_URL = `/${VERSION}/events/traffic`;
-export const ROOT_USERS_URL = `/${VERSION}/users`;
+export const ROOT_AUTH_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/auth`;
+export const ROOT_CONFIG_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/config`;
+export const ROOT_GENRE_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/genres`;
+export const ROOT_LIBRARY_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/library`;
+export const ROOT_PLAYLISTS_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/playlists`;
+export const ROOT_RESOURCES_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/resources`;
+export const ROOT_SHOWS_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/events/shows`;
+export const ROOT_TRAFFIC_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/events/traffic`;
+export const ROOT_USERS_URL =
+  process.env.REACT_APP_API_SERVER_URL + `/${VERSION}/users`;

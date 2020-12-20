49998f9ebcf00c79575faf9fc4c0e1edfed613c6
Kyle Middleton <kylemid321@gmail.com>
2020-12-19 18:01:41 -0700

    incorporate showlistforuser component

--- client/src/components/ScratchpadModal/ScratchpadModal.js
+++ client/src/components/ScratchpadModal/ScratchpadModal.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Modal from '../Modal';
 import Button from '../Button';
+import moment from 'moment';
 import { libraryActions } from '../../redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { showAPI } from '../../api';
+import ShowListForUser from '../ShowListForUser';
 
 class ScratchpadModal extends Component {
   componentWillMount() {
@@ -16,9 +18,18 @@ class ScratchpadModal extends Component {
   }
   render() {
     const { closeScratchpadModal } = this.props;
+    const today = moment();
+    const todayPlus3Months = moment().add('3', 'month');
+    const oneYearAgo = moment().subtract('1', 'year');
     console.log(this.props);
     return (
       <Modal isOpen={true}>
+        <ShowListForUser
+          maxItems="5"
+          startDate={today}
+          endDate={todayPlus3Months}
+          noItemsText="You have no upcoming shows in the next three months."
+        />
         <Button color="neutral" onClick={closeScratchpadModal} className="ml-1">
           Close
         </Button>

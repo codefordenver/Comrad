05e0a409f6dd63504a3ade3585bb6bad7d827a48
Kyle Middleton <kylemid321@gmail.com>
2021-01-16 18:57:20 -0700

    add callback parameter

--- client/src/redux/playlist/actions/findOrCreateOne.js
+++ client/src/redux/playlist/actions/findOrCreateOne.js
@@ -1,13 +1,20 @@
 import { playlistTypes } from '../playlistTypes';
 import { playlistAPI } from '../../../api';
 
-export const findOrCreateOne = (startTime, endTime) => async dispatch => {
+export const findOrCreateOne = (
+  startTime,
+  endTime,
+  callback,
+) => async dispatch => {
   try {
     dispatch({ type: playlistTypes.LOADING });
 
     const doc = await playlistAPI.findOrCreateOne(startTime, endTime);
 
     dispatch({ type: playlistTypes.FIND_ONE, payload: doc.data });
+    if (typeof callback === 'function') {
+      callback(doc.data);
+    }
   } catch (err) {
     console.log('Playlist Find Or Create One Error: ', err);
   }

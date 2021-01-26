d37e9dee909278d4a8502cc6c66b0e0894fbfd98
Kyle Middleton <kylemid321@gmail.com>
2021-01-26 09:43:46 -0700

    remove unneeded imports, code cleanup

--- client/src/components/ScratchpadModal/ScratchpadModal.js
+++ client/src/components/ScratchpadModal/ScratchpadModal.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import Modal from '../Modal';
 import Button from '../Button';
 import moment from 'moment';
-import { libraryActions, playlistActions } from '../../redux';
+import { playlistActions } from '../../redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { showAPI } from '../../api';
 import ShowListForUser from '../ShowListForUser';
 
 class ScratchpadModal extends Component {
@@ -13,7 +12,7 @@ class ScratchpadModal extends Component {
     const { closeScratchpadModal, trackId } = this.props;
     const today = moment();
     const todayPlus3Months = moment().add('3', 'month');
-    const oneYearAgo = moment().subtract('1', 'year');
+    const { addToScratchpadSuccess } = this.props;
     return (
       <Modal isOpen={true}>
         <div className="library-search__scratchpad-modal">
@@ -25,7 +24,7 @@ class ScratchpadModal extends Component {
             trackId={trackId}
             showAddToScratchpadButton={true}
             noItemsText="You have no upcoming shows in the next three months."
-            addToScratchpadSuccess={this.props.addToScratchpadSuccess}
+            addToScratchpadSuccess={addToScratchpadSuccess}
           />
           <Button
             color="neutral"

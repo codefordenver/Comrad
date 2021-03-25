00215dce5dda9086df3f327a75d040ceb8cc381f
Kyle Middleton <kylemid321@gmail.com>
2021-03-24 19:59:43 -0600

    add cancel token to show file

--- client/src/api/show/find.js
+++ client/src/api/show/find.js
@@ -7,6 +7,12 @@ export function find(
   host = null,
   onlyDisplayShowsWithNoHost = false,
 ) {
+  // cancel the previous request, if it exists
+  if (window.activeShowFindAxiosRequest != null) {
+    console.log(window.activeShowFindAxiosRequest);
+    window.activeShowFindAxiosRequest.cancel();
+  }
+
   if (typeof startDate === 'object' && startDate._isAMomentObject) {
     startDate = startDate.toDate();
   }
@@ -20,7 +26,12 @@ export function find(
   if (onlyDisplayShowsWithNoHost) {
     params.showsWithNoHost = true;
   }
+
+  //save the source to a global variable so we can cancel it later, if necessary
+  window.activeShowFindAxiosRequest = axios.CancelToken.source();
+
   return axios.get(ROOT_SHOWS_URL, {
     params: params,
+    cancelToken: window.activeShowFindAxiosRequest.token,
   });
 }

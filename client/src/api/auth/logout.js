962bcf29922f2e5f20f53be27fb512731d9ac93e
Sean Williams <sean@prismtechstudios.com>
2022-08-24 08:18:41 -0500

    Test change to see if cachebuster changes on build

--- client/src/api/auth/logout.js
+++ client/src/api/auth/logout.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { ROOT_AUTH_URL } from '../root';
 
 export function logout() {
-  return axios.get(`${ROOT_AUTH_URL}/logout`);
+  return axios.get(`${ROOT_AUTH_URL}/logout`);  
 }

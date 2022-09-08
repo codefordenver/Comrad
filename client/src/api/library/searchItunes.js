28f0c3e4a7c06d758fbc763d25f4f334cbac7f82
Sean Williams <sean@prismtechstudios.com>
2022-09-08 08:50:57 -0500

    Don't release itunes search yet

--- client/src/api/library/searchItunes.js
+++ client/src/api/library/searchItunes.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { ROOT_LIBRARY_URL } from '../root';
 
 export function searchItunes(type, searchString, limit) {
-  let searchUrl = `${ROOT_LIBRARY_URL}/search-itunes?q=` + encodeURIComponent(searchString);
+  let searchUrl = ROOT_LIBRARY_URL + "/search-itunes?q=" + encodeURIComponent(searchString);
   return axios.get(searchUrl);
 }

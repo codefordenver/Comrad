4355bff5126ebd4023e35a23e945be695f26245f
Kyle Middleton <kylemid321@gmail.com>
2023-10-27 14:43:03 -0600

    add useDefaultDates attribute to component

--- client/src/pages/ReportingUnderwritingPage/ReportingUnderwritingPage.js
+++ client/src/pages/ReportingUnderwritingPage/ReportingUnderwritingPage.js
@@ -67,6 +67,7 @@ class ReportingUnderwritingPage extends Component {
                 to: moment(),
               }}
               withUnderwriterName={true}
+              useDefaultDates={true}
             />
           </>
         )}

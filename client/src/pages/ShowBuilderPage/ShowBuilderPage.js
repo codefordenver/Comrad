e7627f14348355171310c68a63cb198b6a834fb2
Kyle Middleton <kylemid321@gmail.com>
2023-10-30 15:01:08 -0600

    add Loading component to search tab and conditional

--- client/src/pages/ShowBuilderPage/ShowBuilderPage.js
+++ client/src/pages/ShowBuilderPage/ShowBuilderPage.js
@@ -710,6 +710,7 @@ class ShowBuilderPage extends Component {
                   </div>
                   {activeTab === 'search' && (
                     <div className="library-tab-container__tab-content">
+                      {library.loadingSearch && (<Loading />)}
                       <form autoComplete="off" onSubmit={handleSubmit(this.searchLibrary)}>
                         <Field
                           className="mb-1"
